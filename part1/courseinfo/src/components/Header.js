2e0eef3f188be9eb08ffabca9d20c592956d14ef
Jon Vollset <jonv@stacc.com>
2023-05-09 10:00:09 +0200

    This includes part 0 of the course

--- part1/courseinfo/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-const Header = ({ course }) => {
-  return (
-    <header>
-      <h1>{course.name}</h1>
-    </header>
-  );
-};
-
-export default Header;
--- part1/courseinfo/src/components/Part.js
+++
@@ -1,9 +0,0 @@
-const Part = ({ part }) => {
-  return (
-    <p key={part.index}>
-      {part.name} {part.exercises}
-    </p>
-  );
-};
-
-export default Part;
