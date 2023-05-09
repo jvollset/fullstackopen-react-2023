4b67ce8f94e83a4a15d73f9732966c852315117c
Jon Vollset <jonv@stacc.com>
2023-05-09 14:21:34 +0200

    Added coursinfo to the part 2 of the course

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [searchResult, setSearchResults] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+    const personObject = {
+      name: newName,
+      number: newNumber,
+    };
+    setPersons(persons.concat(personObject));
+    setSearchResults(searchResult.concat(personObject));
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearch = (event) => {
+    const search = event.target.value;
+    const filteredPersons = persons.filter((person) =>
+      person.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setSearchResults(filteredPersons);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <h2>Phonebook</h2>
+      filter shown with <input placeholder="search" onChange={handleSearch} />
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number:
+          <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={addPerson}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {searchResult.map((person) => (
+        <p key={person.name}>
+          {person.name} {person.number}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
